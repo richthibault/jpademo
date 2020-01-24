84b99908f1c216b89b71ac34e5b05f78821d3654
Richard Thibault <rich.thibault@gmail.com>
2020-01-23 22:07:40 -0500

    Update PostRepository.java
    removed useless EntityType

--- src/main/java/com/richthibault/jpademo/repo/PostRepository.java
+++ src/main/java/com/richthibault/jpademo/repo/PostRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,15 +13,13 @@ import com.richthibault.jpademo.model.Post;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
-	// use named entity graph from the Post model object
 	@Override
-	@EntityGraph(value="Post.withAuthor",type=EntityGraphType.LOAD)
+	@EntityGraph("Post.withAuthor")
 	Page<Post> findAll(Pageable pageable);
-	
 	
 	// other approaches....
 	
-	// or define an ad hoc entitygraph
+	// define an ad hoc entitygraph
 	@EntityGraph(attributePaths = {"author"})
 	List<Post> findByAuthorLastName(String lastName);
 	
